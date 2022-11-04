90e2acccd0d2780518aa7f589cfa0f66bbd761e4
Jose Antonio <jamacia666@gmail.com>
2022-11-04 19:30:08 +0100

    Actualizar cambios router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'front-end';
+export class AppComponent  implements OnInit{
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.peliculasEnCines = [{
+        titulo: 'Spider-Man',
+        fechaLanzamiento: new Date(),
+        precio: 1400.99
+      },
+      {
+        titulo: 'Moana',
+        fechaLanzamiento: new Date('2016-11-14'),
+        precio: 300.99
+      }
+    ]
+   
+    }, 0o0);
+   
+  }
+  peliculasEnCines;
+  peliculasEnProximosExtrenos=[{
+    titulo: 'Avengers: Endgame',
+    fechaLanzamiento: new Date(),
+    precio: 1400.99
+  },
+  {
+    titulo: 'Inception',
+    fechaLanzamiento: new Date('2016-11-14'),
+    precio: 300.99
+  },
+  {
+    titulo: 'Rocky',
+    fechaLanzamiento: new Date('2016-11-14'),
+    precio: 300.99
+  },
+]
+  
+  title = 'el valor que yo quiera';
+  
+
+
+  duplicarNumero(valor: number): number {
+    return valor * 2;
+
+
+  }
 }
