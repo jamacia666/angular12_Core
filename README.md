b08e327d4344f4651ae5ed3b8dd710b3dbabe7c8
jamacia666 <jamacia666@gmail.com>
2023-05-19 12:32:56 +0200

    first commit

--- README.md
+++ README.md
@@ -28,3 +28,4 @@ To get more help on the Angular CLI use `ng help` or go check out the [Angular C
 "# angular12_Core" 
 "# angular12_Core" 
 "# angular12_Core_Siguiente" 
+"# angular12_Core_Siguiente2" 
---
+++ src/app/generos/crear-genero/crear-genero.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { primeraLetraMayuscula } from 'src/app/utilidades/validadores/primeraLetraMayuscula';
+import { generoCreacionDTO } from '../genero';
+
+@Component({
+  selector: 'app-crear-genero',
+  templateUrl: './crear-genero.component.html',
+  styleUrls: ['./crear-genero.component.css']
+})
+export class CrearGeneroComponent  {
+
+  constructor(private router: Router, private formBuilder: FormBuilder) { }
+
+  guardarCambios(genero: generoCreacionDTO) {
+    // ... guardar los cambios
+    console.log(genero);
+    this.router.navigate(['/generos'])
+  }
+
+}
